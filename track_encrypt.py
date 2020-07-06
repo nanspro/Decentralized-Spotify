f09517a3f91c5570c3303c79570b4dadd1c4cbaf
Arjun Nemani <nemaniarjun@gmail.com>
2020-07-06 15:25:23 +0530

    Add songs and changes

--- track_encrypt.py
+++ track_encrypt.py
@@ -1,5 +1,6 @@
 import os
 import json
+import msgpack
 
 from nucypher.characters.lawful import Enrico
 
@@ -9,28 +10,23 @@ from nucypher.characters.lawful import Enrico
 # Data Sources can produce encrypted data for access policies
 # that **don't exist yet**.
 # In this example, we create a local file with encrypted data
-def encrypt_track_samples(policy_pubkey, path_file):
+def encrypt_track_segments(policy_pubkey, path_file):
     data_source = Enrico(policy_encrypting_key=policy_pubkey)
-
     data_source_public_key = bytes(data_source.stamp)
 
     track_files = os.listdir(path_file)
-    track_segments = list()
-    for file in track_files:
-        with open(file, "rb") as f:
+    for track_segment in track_files:
+        with open(track_segment, "rb") as f:
             plaintext = f.read()
 
         ciphertext, signature = data_source.encrypt_message(plaintext)
-        track_segments.append(ciphertext)
+        
         print("Signature", signature)
-        with open(file + '_encrypted', "wb") as f:
-            f.write(ciphertext)
+        data = {
+            'track_segment_data': ciphertext,
+            'data_source': data_source_public_key
+        }
+        with open(track_segment + '_encrypted', "wb") as f:
+            msgpack.dump(data, f, use_bin_type=True)
     
-    data = {
-        'track_segments': track_segments,
-        'data_source': data_source_public_key
-    }
-    with open('track_segments', "wb") as f:
-        f.write(data)
-
-    return data
+    return True
